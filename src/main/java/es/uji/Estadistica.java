7666cc77ca2feeaa2f085b51455e15b364f9b48f
AlexuJ <al426239@uji.es>
2024-02-11 18:43:24 +0100

    Añado el código de Mario

---
+++ src/main/java/es/uji/Estadistica.java
@@ -0,0 +1,20 @@
+package es.uji;
+public class Estadistica extends Aritmetica {
+    private float ultimoResultado;
+
+
+    public float mediaAritmetica(float[] datos) {
+        return divividir(sumador(datos),datos.length);
+    }
+    public float varianza(float[] datos) {
+        float media = mediaAritmetica(datos);
+        float[] numeros = new float[datos.length];
+        for (int i = 0 ;i<datos.length;i++) {
+            numeros[i] = multiplicar(sumar(datos[i],-media),sumar(datos[i],-media));
+        }
+        return  divividir(sumador(numeros),datos.length);
+    }
+    public double desviacionTipica(float[] datos) {
+        return eleva(varianza(datos),0.5f);
+    }
+}
