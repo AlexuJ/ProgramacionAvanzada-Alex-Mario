e29cd3a3882f241fe9a366be765a0f64eee46667
AlexuJ <al426239@uji.es>
2024-02-20 14:07:00 +0100

    Lista de Rows

--- Practica1/src/main/java/es/uji/Table.java
+++ Practica1/src/main/java/es/uji/Table.java
@@ -1,10 +1,12 @@
 package es.uji;
 
+import java.util.Date;
 import java.util.List;
 
 public class Table {
     public List<String> headers;
+    public List<Row> datos;
     public Row getRowAt(int rowNumber) {
-
+        return datos.get(rowNumber);
     }
 }
