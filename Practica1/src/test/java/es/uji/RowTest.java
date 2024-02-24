fc34de5d8f06a77d219b077014f9c1e3db6b2cbc
Marius <mariodlahorrafalomir@gmail.com>
2024-02-24 20:03:21 +0100

    Pregunta para sven
    Porque aqui necesito oblicatoriamente que los metodos sean estaticos

--- Practica1/src/test/java/es/uji/RowTest.java
+++ Practica1/src/test/java/es/uji/RowTest.java
@@ -1,14 +1,51 @@
 package es.uji;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RowTest {
     //inicio prueba 1 de test
-    @Test
-    @DisplayName("Test de prueba de test")
-    void suma(){
-        assertTrue(true);
+    private  Row fila ;
+    private List<Double> datos;
+    public List<Double> Creadatos(List<Double> datos){
+        for (double i = 0 ; i<5;i++){
+            datos.add(i);
+        }
+        return datos;
     }
+    @BeforeAll
+    void  inicioClase(){
+        fila = new Row();
+        datos = Creadatos(datos);
+    }
+
+    @Test
+    @DisplayName("Almacenamiento")
+    void Prueba1(){
+        fila.data = Creadatos(fila.data);
+        assertArrayEquals(datos.toArray(),fila.data.toArray(),"Se esperava que los arrays coincidieran");
+    }
+    @Test
+    @DisplayName("GET DATA")
+    void Prueba2(){
+        fila.data = Creadatos(fila.data);
+        assertArrayEquals(fila.getData().toArray(),fila.data.toArray());
+
+    }
+    @Test
+    @DisplayName("Excepcion Contra tipos de datos")
+    void Prueba3(){
+
+        fila.data = Creadatos(fila.data);
+        assertArrayEquals(fila.getData().toArray(),fila.data.toArray());
+
+    }
+
+
 }
