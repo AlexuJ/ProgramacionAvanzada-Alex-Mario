f021410042be23f03135999b09bd94b565d97ead
Marius <mariodlahorrafalomir@gmail.com>
2024-02-24 18:13:43 +0100

    Test - Iniciados
    Cabron avisame de la version de java la proxima vez te paso los test inicializados para como que funcionan

--- Practica1/src/test/java/es/uji/TableWithLabelsTest.java
+++ Practica1/src/test/java/es/uji/TableWithLabelsTest.java
@@ -1,7 +1,15 @@
 package es.uji;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TableWithLabelsTest {
-
+    //inicio prueba 1 de test
+    @Test
+    @DisplayName("Test de prueba de test")
+    void suma(){
+        assertTrue(true);
+    }
 }
