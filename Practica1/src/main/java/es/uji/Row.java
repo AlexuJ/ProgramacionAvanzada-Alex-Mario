e2678bf8895a6b31e4e896bd494cd0d32c0fb237
Marius <mariodlahorrafalomir@gmail.com>
2024-02-24 21:18:12 +0100

    Branch porque quiero hacer una prueba
    me voy a cenar pero voy a hacer pruebas implementando mas funcionalidades

--- Practica1/src/main/java/es/uji/Row.java
+++ Practica1/src/main/java/es/uji/Row.java
@@ -4,11 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Row {
+
+    //Atributo
     public List<Double> data;
+    //constructor
     public Row() {
         data = new ArrayList<>();
     }
     public List<Double> getData() {
         return data;
+    }
+    public void setData(List<Double> numero){
+       data = numero ;
+    }
+    public Double ObtenerDato(int numero){
+        if (numero>data.size()-1){
+            return data.get(numero);
+        }else {
+            throw new  IndexOutOfBoundsException("Indice fuera de los limites");
+        }
     }
 }
--- Practica1/src/test/java/es/uji/RowTest.java
+++ Practica1/src/test/java/es/uji/RowTest.java
@@ -1,6 +1,6 @@
 package es.uji;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,42 +10,9 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RowTest {
-    //inicio prueba 1 de test
-    private  Row fila ;
-    private List<Double> datos;
-    public List<Double> Creadatos(List<Double> datos){
-        for (double i = 0 ; i<5;i++){
-            datos.add(i);
-        }
-        return datos;
-    }
-    @BeforeAll
-    void  inicioClase(){
-        fila = new Row();
-        datos = Creadatos(datos);
-    }
+    private Row fila;
 
-    @Test
-    @DisplayName("Almacenamiento")
-    void Prueba1(){
-        fila.data = Creadatos(fila.data);
-        assertArrayEquals(datos.toArray(),fila.data.toArray(),"Se esperava que los arrays coincidieran");
-    }
-    @Test
-    @DisplayName("GET DATA")
-    void Prueba2(){
-        fila.data = Creadatos(fila.data);
-        assertArrayEquals(fila.getData().toArray(),fila.data.toArray());
-
-    }
-    @Test
-    @DisplayName("Excepcion Contra tipos de datos")
-    void Prueba3(){
-
-        fila.data = Creadatos(fila.data);
-        assertArrayEquals(fila.getData().toArray(),fila.data.toArray());
-
-    }
+    @
 
 
 }
