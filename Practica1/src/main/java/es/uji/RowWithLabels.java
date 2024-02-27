8c0db5e4976a952763cecbd4561312bb8fae6c4d
kiwikiwi2002 <38500827+kiwikiwi2002@users.noreply.github.com>
2024-02-27 14:20:59 +0100

    test
    He hecho test y ademas he creado utilidades en la clase RowWithLabels

--- Practica1/src/main/java/es/uji/RowWithLabels.java
+++ Practica1/src/main/java/es/uji/RowWithLabels.java
@@ -3,9 +3,25 @@ package es.uji;
 import java.util.List;
 
 public class RowWithLabels extends Row {
+    //etiqueta convertida en numero
     public int numberClass;
+    //getNumberClass returnea numberClass
     public int getNumberClass() {
         return numberClass;
+    }
+    //getData te da la lista de numeros
+    @Override
+    public List<Number> getData() {
+        return super.getData();
+    }
+    //se invoca al metododo setData de la clase madre
+    @Override
+    public void setData(List<Number> NewData){
+        super.setData(NewData);
+    }
+    //creo el set de NumberClass
+    public void  setNumberClass(int number){
+        numberClass = number ;
     }
 
 }
