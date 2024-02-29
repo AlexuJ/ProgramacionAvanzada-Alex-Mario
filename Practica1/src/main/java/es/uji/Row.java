d6c5591a52a81bd3865f6b8ff745dd3e5135341a
Marius <mariodlahorrafalomir@gmail.com>
2024-02-29 07:52:35 +0100

    version 1.2
    Elimino la interfaz
    añado una nueva funcionalidad llamada setListHeader que hace algo parecido al headers de CSV pero en la tabla y he comenzado a diseñar un CSV que con una funcion lea los ficheros independientemente del parametro que se le pase

--- Practica1/src/main/java/es/uji/Row.java
+++ Practica1/src/main/java/es/uji/Row.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Row {
     // se almacena una list de cualquier dato numerico que necesitemos
     // usamos la interfaz Number y List para poder obtener flexivilidad en codigo
-    public List<Double> data;
+    public List<Number> data;
 
     // constructor de la clase crea una ArrayList
     public Row() {
@@ -15,16 +15,16 @@ public class Row {
     }
 
     // devuelve data
-    public List<Double> getData() {
+    public List<Number> getData() {
         return data;
     }
 
     // pasar una lista al atributo data
-    public void setData(List<Double> newData) {
+    public void setData(List<Number> newData) {
         data = newData;
     }
 
-    public void setUnicData(Double nuevoNumero) {
+    public void setUnicData(Number nuevoNumero) {
         data.add(nuevoNumero);
     }
 }
