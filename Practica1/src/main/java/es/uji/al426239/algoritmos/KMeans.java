654ec48bbf9579c979fd48c5a9e901cd98fab183
Marius <mariodlahorrafalomir@gmail.com>
2024-04-12 17:55:58 +0200

    Fixeando el error del csv
    Encapsulacion con existo el problema era la ruta relativa del data del song por eso no iba confirmame que te va

--- Practica1/src/main/java/es/uji/al426239/algoritmos/KMeans.java
+++ Practica1/src/main/java/es/uji/al426239/algoritmos/KMeans.java
@@ -6,12 +6,13 @@ import es.uji.al426239.row_table.Table;
 import es.uji.al426239.metodos.Convertidor;
 import java.util.*;
 
-public class KMeans implements Algorithm<Table,List<Number>,Integer> {
+public class KMeans implements Algorithm<Table, List<Number>, Integer> {
     private int numClusters;
-    private  int numIterations;
-    private   List<Row> Representantes;
-    private   long seed;
-    private   Map<Integer,List<Row>> Grupos;
+    private int numIterations;
+    private List<Row> Representantes;
+    private long seed;
+    private Map<Integer, List<Row>> Grupos;
+
     public KMeans(int numClusters, int numIterations, long seed) {
         this.numClusters = numClusters;
         this.numIterations = numIterations;
@@ -19,22 +20,26 @@ public class KMeans implements Algorithm<Table,List<Number>,Integer> {
         this.Representantes = new ArrayList<>();
         this.Grupos = new HashMap<>();
     }
+
     @Override
     public void train(Table datos) {
         inicializar(datos);
-        for (int i=0; i < numIterations; i++) {
+        for (int i = 0; i < numIterations; i++) {
             Grupos.clear();
             for (Row fila : datos.getRow()) {
-                //si el resultado del stimate esta añade no hace nada si no existe la clave añade la fila
+                // si el resultado del stimate esta añade no hace nada si no existe la clave
+                // añade la fila
                 Grupos.computeIfAbsent(estimate(fila.getData()), k -> new ArrayList<>()).add(fila);
             }
-           Operaciones calculador = new Operaciones();
-            Representantes = calculador.calcularCentroides(datos,numClusters,Grupos,Representantes);
+            Representantes.clear();
+            Operaciones calculador = new Operaciones();
+            Representantes = calculador.calcularCentroides(datos, numClusters, Grupos, Representantes);
         }
     }
+
     private void inicializar(Table datos) {
         Random random = new Random(seed);
-        for (int i=0; i < numClusters; i++) {
+        for (int i = 0; i < numClusters; i++) {
             int fila = random.nextInt(datos.getRow().size());
             if (!Representantes.contains(datos.getRow(fila))) {
                 Representantes.add(datos.getRow(fila));
@@ -42,14 +47,16 @@ public class KMeans implements Algorithm<Table,List<Number>,Integer> {
             Grupos.put(i, new ArrayList<>());
         }
     }
+
     @Override
     public Integer estimate(List<Number> dato) {
         Operaciones calculador = new Operaciones();
         Convertidor convertidor = new Convertidor();
         double menor = Double.MAX_VALUE;
         int grupo = 0;
-        for (int i=0; i < Representantes.size(); i++) {
-            double cercania = calculador.CalcularMetricaEuclidiana(convertidor.convertirADouble(dato), Representantes.get(i));
+        for (int i = 0; i < Representantes.size(); i++) {
+            double cercania = calculador.CalcularMetricaEuclidiana(convertidor.convertirADouble(dato),
+                    Representantes.get(i));
             if (cercania < menor) {
                 menor = cercania;
                 grupo = i;
--- Practica1/src/test/java/es/uji/al426239/KMeansTest.java
+++ Practica1/src/test/java/es/uji/al426239/KMeansTest.java
@@ -6,10 +6,6 @@ import es.uji.al426239.algoritmos.KMeans;
 import es.uji.al426239.lector_de_tablas.CSV;
 import es.uji.al426239.row_table.Row;
 import es.uji.al426239.row_table.Table;
-<<<<<<< HEAD
-import es.uji.al426239.sistema_de_recomendacion.RecSys;
-=======
->>>>>>> 3d9dddb1a5e281a1a39581f2712fb5f0f3e2b6c1
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
@@ -17,25 +13,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 class KMeansTest {
-    private KMeans kmeans ;
+    private KMeans kmeans;
     private List<List<Number>> parametrosprueba;
-    private  Integer numeroclusters;
-    private  Integer iteraciones;
-    private  Long semilla;
+    private Integer numeroclusters;
+    private Integer iteraciones;
+    private Long semilla;
     private Table tabla;
     private CSV lector;
 
     @BeforeEach
     void inicioClase() throws FileNotFoundException {
-       String separator = System.getProperty("file.separator");
-       lector = new CSV();
-       tabla = lector.readTable("."+ separator + "data"+ separator +"songs_train_withoutnames.csv");
-       parametrosprueba = new ArrayList<>();
-       semilla = 4321L;
-       numeroclusters = 15;
-       iteraciones = 200;
-       kmeans = new KMeans(numeroclusters, iteraciones, semilla);
+        String separator = System.getProperty("file.separator");
+        lector = new CSV();
+        tabla = lector.readTable("." + separator + "data" + separator + "songs_train_withoutnames.csv");
+        parametrosprueba = new ArrayList<>();
+        semilla = 4321L;
+        numeroclusters = 15;
+        iteraciones = 200;
+        kmeans = new KMeans(numeroclusters, iteraciones, semilla);
     }
+
     @Test
     void train() {
         kmeans.train(tabla);
