ae595a873c773b0ddcdd5dd58149f717bf40ff8e
Marius <mariodlahorrafalomir@gmail.com>
2024-04-14 18:52:43 +0200

    ULTIMO COMIT
    YA ACABAMOS

--- Practica1/src/test/java/es/uji/al426239/KNNTest.java
+++ Practica1/src/test/java/es/uji/al426239/KNNTest.java
@@ -20,36 +20,38 @@ class KNNTest {
         private List<Integer> Datos6;
 
         @BeforeEach
-        void inicioClase() {
-                try {
-                        CalcularMetricaEuclidiana = new Operaciones();
-                        CSV lector = new CSV();
-                        Estimador = new KNN();
-                        String separator = System.getProperty("file.separator");
-                        tablaEntrenamiento = lector.readTableWithLabels("." + separator + "iris.csv");
-                        Estimador.train(tablaEntrenamiento);
-                        Datos1 = new ArrayList<>(List.of(4.2, 2.6, 1.2, 0.7));
-                        Datos2 = new ArrayList<>(List.of(7.2, 5.6, 2.5, 1.9));
-                        Datos3 = new ArrayList<>(List.of(0.0, 0.0, 0.0, 0.0));
-                        Datos4 = new ArrayList<>(List.of(10.2, 7.6, 4.2, 2.7));
-                        Datos5 = new ArrayList<>(List.of(6.2, 4.5, 3.5, 1.5));
-                        Datos6 = new ArrayList<>(List.of(1, 2, 3, 4));
-                } catch (FileNotFoundException e) {
-                        // Manejar la excepción adecuadamente
-                        e.printStackTrace();
-                }
+        void inicioClase() throws FileNotFoundException {
+
+                CalcularMetricaEuclidiana = new Operaciones();
+                CSV lector = new CSV();
+                Estimador = new KNN();
+                String separator = System.getProperty("file.separator");
+                tablaEntrenamiento = lector.readTableWithLabels("." + separator + "iris.csv");
+                Estimador.train(tablaEntrenamiento);
+                Datos1 = new ArrayList<>(List.of(4.2, 2.6, 1.2, 0.7));
+                Datos2 = new ArrayList<>(List.of(7.2, 5.6, 2.5, 1.9));
+                Datos3 = new ArrayList<>(List.of(0.0, 0.0, 0.0, 0.0));
+                Datos4 = new ArrayList<>(List.of(10.2, 7.6, 4.2, 2.7));
+                Datos5 = new ArrayList<>(List.of(6.2, 4.5, 3.5, 1.5));
+                Datos6 = new ArrayList<>(List.of(1, 2, 3, 4));
+                // variable de error
+                double delta = 0.001;
         }
 
         @DisplayName("Devuelve TablaEntrenamiento")
         @Test
         void getTablaEntramiento() {
-                //variable de error
-                double delta = 0.001;
-                Assertions.assertEquals(0.592, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos1, tablaEntrenamiento.getRow(41)), delta,"Debería dar 0.592");
-                Assertions.assertEquals(4.019, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos2, tablaEntrenamiento.getRow(1)), delta,"Debería dar 4,019");
-                Assertions.assertEquals(7.246, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos3, tablaEntrenamiento.getRow(79)), delta,"Debería dar 7,246");
-                Assertions.assertEquals(6.722, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos4, tablaEntrenamiento.getRow(121)), delta,"Debería dar 6,722");
-                Assertions.assertEquals(2.341, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos5, tablaEntrenamiento.getRow(147)), delta,"Debería dar 2,341");
+
+                Assertions.assertEquals(0.592, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos1,
+                                tablaEntrenamiento.getRow(41)), delta, "Debería dar 0.592");
+                Assertions.assertEquals(4.019, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos2,
+                                tablaEntrenamiento.getRow(1)), delta, "Debería dar 4,019");
+                Assertions.assertEquals(7.246, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos3,
+                                tablaEntrenamiento.getRow(79)), delta, "Debería dar 7,246");
+                Assertions.assertEquals(6.722, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos4,
+                                tablaEntrenamiento.getRow(121)), delta, "Debería dar 6,722");
+                Assertions.assertEquals(2.341, CalcularMetricaEuclidiana.CalcularMetricaEuclidiana(Datos5,
+                                tablaEntrenamiento.getRow(147)), delta, "Debería dar 2,341");
         }
 
         @DisplayName("Estimación")
