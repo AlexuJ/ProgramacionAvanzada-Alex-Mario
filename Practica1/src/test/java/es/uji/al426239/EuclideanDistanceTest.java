ffe5d9557cea0a4f5e0f09718d83ad386c646bce
AlexuJ <al426239@uji.es>
2024-04-27 17:42:24 +0200

    Retoco tests distancias

--- Practica1/src/test/java/es/uji/al426239/EuclideanDistanceTest.java
+++ Practica1/src/test/java/es/uji/al426239/EuclideanDistanceTest.java
@@ -1,23 +1,15 @@
 package es.uji.al426239;
 
 import org.junit.jupiter.api.Test;
-
 import es.uji.al426239.distance.Distance;
 import es.uji.al426239.distance.EuclideanDistance;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 
 class EuclideanDistanceTest {
     private Distance ecludiana;
-    private Double resultado1;
-    private Double resultado2;
-    private Double resultado3;
-    private Double resultado4;
     private List<Double> resultados;
     private List<Double> parametros1;
     private List<Double> parametros2;
@@ -34,10 +26,10 @@ class EuclideanDistanceTest {
         parametros3 = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
 
         // resultados
-        resultado1 = 0.0;
-        resultado2 = 6.32;
-        resultado3 = 0.0;
-        resultado4 = 0.0;
+        Double resultado1 = 0.0;
+        Double resultado2 = 6.32;
+        Double resultado3 = 0.0;
+        Double resultado4 = 0.0;
         resultados = Arrays.asList(resultado1, resultado2, resultado3, resultado4);
 
     }
@@ -51,9 +43,7 @@ class EuclideanDistanceTest {
         assertEquals(resultados.get(1), ecludiana.calculateDistance(parametros2, parametros1), error);
 
         // excepcion
-        assertThrows(IllegalArgumentException.class, () -> {
-            ecludiana.calculateDistance(parametros1, parametros3);
-        }, "Debería lanzar IllegalArgumentException si los parámetros son incorrectos");
+        assertThrows(IllegalArgumentException.class, () -> ecludiana.calculateDistance(parametros1, parametros3), "Debería lanzar IllegalArgumentException si los parámetros son incorrectos");
 
     }
 }
--- Practica1/src/test/java/es/uji/al426239/ManhattanDistanceTest.java
+++ Practica1/src/test/java/es/uji/al426239/ManhattanDistanceTest.java
@@ -1,13 +1,9 @@
 package es.uji.al426239;
 
 import org.junit.jupiter.api.Test;
-
 import es.uji.al426239.distance.Distance;
-import es.uji.al426239.distance.EuclideanDistance;
 import es.uji.al426239.distance.ManhattanDistance;
-
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,35 +11,27 @@ import org.junit.jupiter.api.BeforeEach;
 
 class ManhattanDistanceTest {
     private Distance manhattan;
-    private Double resultado1;
-    private Double resultado2;
-    private Double resultado3;
-    private Double resultado4;
     private List<Double> resultados;
     private List<Double> parametros1;
     private List<Double> parametros2;
     private List<Double> parametros3;
     private List<Double> parametros4;
 
-    private Double error;
-
     @BeforeEach
     void inicioClase() {
         manhattan = new ManhattanDistance();
         // parametros
-        error = 0.1;
         parametros1 = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0);
         parametros2 = Arrays.asList(5.0, 4.0, 3.0, 2.0, 1.0);
         parametros3 = Arrays.asList(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
         parametros4 = Arrays.asList(4.0, 2.0, 3.0, 1.0, 2.0);
 
         // resultados
-        resultado1 = 0.0;
-        resultado2 = 0.0;
-        resultado3 = -3.0;
-        resultado4 = 0.0;
+        Double resultado1 = 0.0;
+        Double resultado2 = 0.0;
+        Double resultado3 = -3.0;
+        Double resultado4 = 0.0;
         resultados = Arrays.asList(resultado1, resultado2, resultado3, resultado4);
-
     }
 
     @Test
@@ -57,9 +45,6 @@ class ManhattanDistanceTest {
         assertEquals(resultados.get(2), manhattan.calculateDistance(parametros4, parametros1));
 
         // excepcion
-        assertThrows(IllegalArgumentException.class, () -> {
-            manhattan.calculateDistance(parametros1, parametros3);
-        }, "Debería lanzar IllegalArgumentException si los parámetros son incorrectos");
-
+        assertThrows(IllegalArgumentException.class, () -> manhattan.calculateDistance(parametros1, parametros3), "Debería lanzar IllegalArgumentException si los parámetros son incorrectos");
     }
 }
